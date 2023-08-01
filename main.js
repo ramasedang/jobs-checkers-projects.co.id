5a9edeca9aa068022af4b32376b55fad800eea0d
ssudo <ramasedang@gmail.com>
2023-08-01 22:24:31 +0700

    fix: short by newest

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ import parsingJobs from "./service/project.co.id.js";
 
 // funsi untuk run setiap 5 menit
 const run = async () => {
+  console.log("run at " + new Date().toLocaleString("id-ID") + " WIB");
   await parsingJobs();
-  // setTimeout(run, 300000);
+  setTimeout(run, 300000);
 };
 
 const main = async () => {
